4caaf0de65dcb467a9079f4c28f8edca94c38e34
Carloz <carlos.f@estudante.ifro.edu.br>
2022-10-04 08:08:00 -0400

    usercontrolmenuitem

--- LocaCar/UserControlMenuItem.xaml.cs
+++ LocaCar/UserControlMenuItem.xaml.cs
@@ -24,5 +24,10 @@ namespace LocaCar
         {
             InitializeComponent();
         }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
